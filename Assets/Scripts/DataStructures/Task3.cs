eb9639085a99844999a95382bfbe8fd7fa0e3875
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-02-22 13:19:01 +0000

    added task 3 script for week 5
    added task 3 script for week 5

---
+++ Assets/Scripts/DataStructures/Task3.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task3 : MonoBehaviour
+{
+    //try list or dictionary
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+
+              /*  if (i == 0 && j == 0)
+                {
+                    int plus = i + j;
+                    Debug.Log("plus = " + plus);
+                }
+                if (i == 1 && j == 1)
+                {
+                    int minus = i - j;
+                    Debug.Log("minus = " + minus);
+                }
+                if (i == 2 && j == 2)
+                {
+                    int multiply = i * j;
+                    Debug.Log("multiply = " + multiply);
+                }
+                if (i == 3 && j == 3)
+                {
+                    int divide = i / j;
+                    Debug.Log("divide = " + divide);
+                } */
+    }
+}
+    
+
